f86c43421ae8a5b6e563484eaa1dafd7791b17b8
Ari Morcos <arimorcos@gmail.com>
2015-11-23 18:34:38 -0800

    Infrastructure for logging complete. Just need to add automatic logging after each epoch.
    After a network object is instantiated, user must call model.set_log_dir() to specify the logging directory. Model will intelligently handle a non-empty directory being present. The current log directory can be retrieved using model.get_log_dir(). Once the logging directory has been specified, the model specifications (input sizes, layers, etc.) can be saved using model.save_model_specs(). The parameters of all shared variables can be saved using model.write_parameters().
    
    To set parameters to values stored in a file, call model.set_parameters(epoch=?, param_file=?). Only specify one of epoch and param_file. If the epoch parameter is specified, model will look for a file corresponding to the given epoch in the log directory. param_file can be used to specify an arbitrary file. Once file has been specified, set_parameters will load file and set parameters accordingly.
    
    Finally, added method to save the model. model.save_model() will save the entire model to a file. This will be useful if we don't want to recompile every time.

--- theano_lstm.py
+++ theano_lstm.py
@@ -2,7 +2,11 @@ import theano
 import theano.tensor as T
 from lstm_network_components import LSTM_stack, soft_reader
 from lstm_optimizers import adadelta_fears_committment, adam_loves_theano
-import csv
+import sys
+import cPickle
+import os
+import re
+import warnings
 
 
 class lstm_rnn:
@@ -20,6 +24,9 @@ class lstm_rnn:
         for L in layer_spec_list:
             LSTM_out_size += L[1]
 
+        # store output size
+        self.final_output_size = final_output_size
+
         # Initialize weights
         self.LSTM_stack.initialize_stack_weights()
 
@@ -32,7 +39,8 @@ class lstm_rnn:
         # initialize the training functions
         self.initialize_training_functions()
 
-        self.log_file = None
+        self.log_dir = None
+        self.curr_epoch = 0
 
     def create_network_graph(self):
 
@@ -75,33 +83,160 @@ class lstm_rnn:
     def initialize_network_weights(self):
         self.LSTM_stack.initialize_stack_weights()
 
-    def set_log_file(self, log_file):
+    def set_log_dir(self, log_dir):
         """
-        Sets the current log file
-        :param log_file: path to log file
+        Sets the current log directory
+        :param log_dir: path to log directory
         :return: None
         """
-        self.log_file = log_file
+        self.log_dir = log_dir
 
-    def get_log_file(self):
+        if os.path.isdir(log_dir):
+            # Get file list
+            file_list = os.listdir(log_dir)
+
+            # Subset to epoch files
+            epoch_string = 'Epoch_\d{4}_weights'
+            epoch_files = [item for item in file_list if re.search(epoch_string, item)]
+
+            # Get most up to date epoch and ask if files are present
+            if epoch_files:
+
+                # Get list of epoch strings
+                epoch_nums = []
+                for x in epoch_files:
+                    match = re.search('((?<=Epoch\_)(\d{4})(?=\_weights))', x)
+                    if match:
+                        epoch_nums.append(int(match.group()))
+
+                # get maximum epoch
+                max_epoch = max(epoch_nums)
+
+                # ask user
+                answer = raw_input("Epoch files already exist. Maximum epoch is {}. Reset y/n?".format(max_epoch))
+                if answer == 'y':
+                    # Delete files
+                    [os.remove(os.path.join(log_dir, x)) for x in epoch_files]
+                elif answer == 'n':
+                    # Set current epoch to max_epoch + 1
+                    self.curr_epoch = max_epoch + 1
+                else:
+                    raise BaseException("Cannot parse answer.")
+
+        else:
+            os.mkdir(log_dir)
+
+    def get_log_dir(self):
         """
-        :return: Path to current log file
+        :return: Path to current log directory
         """
-        return self.log_file
+        if self.log_dir:
+            return self.log_dir
+        else:
+            print "No log directory set."
 
     def write_parameters(self):
-        if self.log_file is None:
-            raise AttributeError("No log file created")
+        """
+        Writes the parameters to a new file in the current log folder
+        :return: None
+        """
+        if self.log_dir is None:
+            raise AttributeError("No log folder specified")
 
-        with open(self.log_file, 'a+') as file_object:
-            writer = csv.writer(file_object, delimiter=',')
-            # writer.writerow()
+        # Create log file
+        log_file = os.path.join(self.log_dir, "Epoch_{:04d}_weights".format(self.curr_epoch))
+
+        with open(log_file, 'wb') as f:
+            for obj in self.LSTM_stack.list_params():
+                cPickle.dump(obj.get_value(), f, protocol=cPickle.HIGHEST_PROTOCOL)
+
+    def set_parameters(self, param_file=None, epoch=None):
+        """
+        Loads parameters from the specified file and sets them
+        :param param_file: file to load parameters from. If specified, loads a specific file.
+        :param epoch: Epoch to load from. If specified, loads parameters from a given epoch in the current log
+        folder. If both param_file and epoch provided, param_file will be used.
+        :return: None
+        """
+
+        # Get load file
+        if param_file:
+            load_file = param_file
+            if epoch:
+                warnings.warn('Both param_file and epoch provided. Using param_file...')
+            if not os.path.isfile(load_file):
+                raise IOError('File {} not found. No parameters have been set'.format(load_file))
+        elif epoch is not None:
+            load_file = os.path.join(self.log_dir, 'Epoch_{:04d}_weights'.format(epoch))
+            if not os.path.isfile(load_file):
+                raise IOError('File corresponding to epoch {}: \"Epoch_{:04d}_weights\" not found.'.format(epoch,
+                                                                                                           epoch))
+        else:
+            raise ValueError('Must provide param_file or epoch. No parameters have been set.')
+
+        # load parameters
+        with open(load_file, 'rb') as f:
+            loaded_objects = []
+            for obj in range(len(self.LSTM_stack.list_params())):
+                loaded_objects.append(cPickle.load(f))
+
+        # set parameters
+        for ind, obj in enumerate(self.LSTM_stack.list_params()):
+            obj.set_value(loaded_objects[ind])
+
+    def save_model(self, save_file):
+        """
+        Saves the entire network model
+        :param save_file: path to file to save
+        :return: None
+        """
+
+        # Get old recursion limit and set recursion limit to very high
+        old_recursion_limit = sys.getrecursionlimit()
+        sys.setrecursionlimit(int(1e5))
+
+        # save the model
+        with open(save_file, mode='wb') as f:
+            cPickle.dump(self, f, protocol=cPickle.HIGHEST_PROTOCOL)
+
+        # Reset old recursion limit
+        sys.setrecursionlimit(old_recursion_limit)
+
+    def save_model_specs(self, save_file="model_specs"):
+        """
+        Saves the specifications of the model: final output size, and for each layer: number of hidden units,
+        layer input size, and layer output size
+        :param save_file: path to file to save
+        :return: None
+        """
+
+        # Check that the log directory is set
+        if self.log_dir is None:
+            raise AttributeError("No log folder specified")
+
+        # Join path to log directory
+        save_file = os.path.join(self.log_dir, save_file)
+
+        # Constructe dictionary of model parameters
+        layer_params = []
+        for layer in self.LSTM_stack.layers:
+            layer_params.append({'num_hidden': layer.num_hidden,
+                                 'num_outputs': layer.num_outputs,
+                                 'num_inputs': layer.num_inputs})
+
+        save_dict = {'final_output_size': self.final_output_size,
+                     'layer_params': layer_params}
+
+        with open(save_file, mode='wb') as f:
+            cPickle.dump(save_dict, f, protocol=cPickle.HIGHEST_PROTOCOL)
 
     # These functions implements that sequential calling into one training step:
     def adam_step(self, S, T):
         self.__f_adam_helpers(S, T)
+        self.curr_epoch += 1
         return self.__f_adam_train(S, T)
 
     def adadelta_step(self, S, T):
         self.__f_adadelta_helpers(S, T)
+        self.curr_epoch += 1
         return self.__f_adadelta_train(S, T)
